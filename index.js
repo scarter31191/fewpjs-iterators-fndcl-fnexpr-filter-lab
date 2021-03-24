4602ff9e317c08374e9f067de96836a2b329fb7d
scarter31191 <scarter31191@gmaile.com>
2021-03-23 20:25:11 -0400

    commit tree 6debf1be25d1a54642d407915f261726f7dfd6e8

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers, target) {
+    return drivers.filter(t => {return t.toLowerCase() === target.toLowerCase()})
+}
+
+function fuzzyMatch(drivers, target) {
+    return drivers.filter(t => {return t.startsWith(target)})
+}
+
+function matchName(drivers, target) {
+    return drivers.filter(t => {return t.name === target})
+}
